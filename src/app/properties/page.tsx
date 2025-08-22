ba2cec3727ab2a7d6d96d2d085d1f8be934cde0d
Wallace Assis <atendimento@tattooai.online>
2025-08-22 12:43:35 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/app/properties/page.tsx
+++ src/app/properties/page.tsx
@@ -14,7 +14,7 @@ const properties = [
   },
   {
     id: 2,
-    imageUrl: "https://placehold.co/600x400.png",
+    imageUrl: "https://images.unsplash.com/photo-1502005097973-6a7082348e28?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxN3x8Q2FzYXxlbnwwfHx8fDE3NTU4NjY1MjR8MA&ixlib=rb-4.1.0&q=80&w=1080",
     hint: "cozy house",
     price: "R$ 1.200.000",
     address: "Av. Paulista, 500, Bela Vista",
