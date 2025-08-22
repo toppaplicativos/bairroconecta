9139ffac42afa15a9cb127ad2f513c161d5f045f
Wallace Assis <atendimento@tattooai.online>
2025-08-22 11:50:14 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -13,7 +13,7 @@ export type Business = {
 export const businesses: Business[] = [
   {
     id: 1,
-    imageUrl: "https://placehold.co/600x400.png",
+    imageUrl: "https://images.unsplash.com/photo-1592861956120-e524fc739696?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxN3x8UmVzdGF1cmFudHxlbnwwfHx8fDE3NTU4NjMzNzV8MA&ixlib=rb-4.1.0&q=80&w=1080",
     hint: "cozy restaurant",
     name: "Cantina da Nona",
     category: "Restaurantes",
