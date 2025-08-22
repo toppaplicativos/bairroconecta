66400648ddaf0fd19f35ae6dec07e7b763717c11
Wallace Assis <atendimento@tattooai.online>
2025-08-22 12:46:22 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/app/classifieds/page.tsx
+++ src/app/classifieds/page.tsx
@@ -12,7 +12,7 @@ const ads = [
   },
   {
     id: 2,
-    imageUrl: "https://placehold.co/600x400.png",
+    imageUrl: "https://images.unsplash.com/photo-1540574163026-643ea20ade25?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwzfHxTb2YlQzMlQTElMjB8ZW58MHx8fHwxNzU1ODY2NzY1fDA&ixlib=rb-4.1.0&q=80&w=1080",
     hint: "sofa",
     title: "Sofá 3 lugares, bom estado",
     price: "R$ 300,00",
